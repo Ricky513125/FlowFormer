ae71318c219a072837499acdab1f4a8c40c54b85
Ricky513125 <lilingyu513125@163.com>
2025-03-12 17:01:37 +0800

    cnt < 4886 skip

--- gen_flow.py
+++ gen_flow.py
@@ -44,7 +44,7 @@ def process_videos(input_root, output_root, model):
     cnt = 0
     for video_folder in sorted(os.listdir(input_root)):
         cnt+=1
-        if cnt < 4858:
+        if cnt < 4886:
             continue
         video_path = os.path.join(input_root, video_folder)
         if not os.path.isdir(video_path):
